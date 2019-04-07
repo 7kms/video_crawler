dbed4716995aada2dcbfecda7d98f315af100995
tangliang <tangliang@hinterstellar.com>
2019-04-07 17:30:04 +0800

    chore: change cover script

--- update/addcover.js
+++ update/addcover.js
@@ -1,4 +1,4 @@
-process.env.NODE_ENV = 'production'
+// process.env.NODE_ENV = 'production'
 
 const Model = require('../src/model/yaoshe');
 const config = require('../config')
@@ -10,21 +10,18 @@ const getList = async(limit,skip)=>{
 }
 
 const getCoverUrl = async (pageurl)=>{
-        var reg = /https:\/\/[^/]+/;
-        let dom = await $get(pageurl);
-        const regCover = /preview_url:.*?'(.*?)'/;
-        let res2 = dom.match(regCover);
-
-        let cover_url = ''
-        if(res2){
-            cover_url = res2[1]
-        }
-       return cover_url.replace(reg,'')
+    let reg = /(\d+)$/;
+    let arr = reg.exec(pageurl);
+    if(arr[1]){
+        return `/contents/videos_screenshots/${parseInt(arr[1]/1000)*1000}/${arr[1]}/preview.mp4.jpg`
+    }else{
+        return ''
+    }
+    
 }
 
 const run = async ()=>{
     await Model.init();
-    const baseurl = config.source.yaoshe.slice(0,-1);
     let limit = 50;
     let skip = 0;
     let list = await getList(limit,skip);
@@ -38,16 +35,11 @@ const run = async ()=>{
             list = await getList(limit,skip);
             console.log(list.length)
         }
-        if(item.cover_url){
-            continue
-        }else{
-            let embedPage = `${baseurl}${item.embed_url}`;
-            // console.log(`${list.length}, going to crawler ${embedPage}`)
-            item.cover_url = await getCoverUrl(embedPage);
-            // console.log(`crawler successful ${embedPage} `)
-            await Model.update(item);
-            console.log(`${list.length}, update successful,  cover=${item.cover_url}`)
-        }
+         // console.log(`${list.length}, going to crawler ${embedPage}`)
+         item.cover_url = await getCoverUrl(item.embed_url);
+         // console.log(`crawler successful ${embedPage} `)
+         await Model.update(item);
+         console.log(`${list.length}, update successful,  cover=${item.cover_url}`)
     }
 
     process.exit(0)
